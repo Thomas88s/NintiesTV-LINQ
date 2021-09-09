4de0713f0b6093dea96051bce8c0045a4f2c2d46
Thomas Witherow <twitherow@bellsouth.net>
2021-09-09 12:03:10 -0500

    yeah...

--- Program.cs
+++ Program.cs
@@ -50,21 +50,22 @@ namespace NinetiesTV
         }
 
         // 2. Return a list of show names ordered alphabetically.
-        static List<string> NamesAlphabetically(List<Show> shows)
+        static List<String> NamesAlphabetically(List<Show> shows)
         {
-            throw new NotImplementedException();
+           return shows.Select(s => s.Name).OrderBy(s => s).ToList();
+            //    throw new NotImplementedException();
         }
 
         // 3. Return a list of shows ordered by their IMDB Rating with the highest rated show first.
         static List<Show> ShowsByPopularity(List<Show> shows)
         {
-            throw new NotImplementedException();
+            return  shows.OrderByDescending(s => s.ImdbRating).ToList();
         }
 
         // 4. Return a list of shows whose title contains an & character.
         static List<Show> ShowsWithAmpersand(List<Show> shows)
         {
-            throw new NotImplementedException();
+           
         }
 
         // 5. Return the most recent year that any of the shows aired.
