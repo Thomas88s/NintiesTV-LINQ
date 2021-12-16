316e89a4e00a77fb5416bddf6aa1e3b1bd6781b9
Thomas Witherow <twitherow@bellsouth.net>
2021-12-16 16:25:50 -0600

    update

--- Program.cs
+++ Program.cs
@@ -127,7 +127,7 @@ namespace NinetiesTV
         // 14. Return the shows with more than one genre ordered by their starting year.
         static List<Show> WithMultipleGenresByStartYear(List<Show> shows)
         {
-            return shows.Where(s => s.Genres > 1).OrderByDescending(s => s.StartYear).Select(s => s.Name).ToList();
+            return shows.Where(s => s.Genres.Count > 1).OrderByDescending(s => s.StartYear).ToList();
         }
 
         // 15. Return the show with the most episodes.
